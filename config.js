1ffd54c6ec48df995cd19a48c55fee4ca598031b
JeffreyBalaoro <balaorojeffrey218@gmail.com>
2022-12-23 18:40:14 +0800

    Added Changes

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "96ff6217cdaf0f3fa5a10600e0605a22"
+    }
+}
+
+module.exports = constants;
